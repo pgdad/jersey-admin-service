0211f378a2fa065dcdf21e5eef736e57988244fb
pgdad <pgdad@hotmail.com>
2012-04-15 15:11:04 -0400

    upgrade version to 1.0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jersey-admin-service "1.0.4"
+(defproject jersey-admin-service "1.0.5"
   :description "FIXME: write description"
   :aot [jerseyAdminService.AdminService jerseyAdminService.adm]
   :repl-init jerseyAdminService.AdminService
