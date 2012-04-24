a4e424e22012409f09e0ae9453de12ad99bb7d67
pgdad <pgdad@hotmail.com>
2012-04-24 17:23:02 -0400

    new version of jetty

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jersey-admin-service "1.0.6"
+(defproject jersey-admin-service "1.0.7"
   :description "FIXME: write description"
   :aot [jerseyAdminService.AdminService jerseyAdminService.adm]
   :repl-init jerseyAdminService.AdminService
@@ -8,7 +8,7 @@
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.json "0.1.3"]
                  [log4j/log4j "1.2.16"]
-                 [jersey-service/jersey-service "1.0.3"]
-                 [jersey-zoo-clj/jersey-zoo-clj "1.0.1"]
-                 [org.eclipse.jetty/jetty-servlet "8.1.2.v20120308"]])
-;;  :dev-dependencies [[org.eclipse.jetty/jetty-servlet "8.1.2.v20120308"]]  )
+                 [jersey-service/jersey-service "1.0.4"]
+                 [jersey-zoo-clj/jersey-zoo-clj "1.0.2"]
+                 [org.eclipse.jetty/jetty-servlet "8.1.3.v20120416"]])
+;;  :dev-dependencies [[org.eclipse.jetty/jetty-servlet "8.1.3.v20120416"]]  )
